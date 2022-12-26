3636b50964f06db83cd9efb86809c7609698280a
jedzill4 <raulonco@gmail.com>
2022-12-26 18:35:52 +0000

    added datetime format suggestions

---
+++ src/aymurai/aymurai/transforms/datetime_formatter/patterns.py
@@ -0,0 +1,23 @@
+import locale
+
+# set locale to es_AR to handle month names
+locale.setlocale(locale.LC_ALL, "es_AR.UTF-8")
+
+DATES = [
+    r"%-d/%-m/%Y",
+    r"%-d/%-m/%y",
+    r"%d/%m/%Y",
+    r"%d/%m/%y",
+    r"(?i)%-d de %B del? %Y",
+]
+HOURS = [
+    r"%H[\.:]%M",
+    r"(?i)%-H[\.:]%M horas",
+    r"(?i)%-H.%M h(rs|r|s)\.?",
+]
+
+patterns = {
+    "FECHA_RESOLUCION": DATES,
+    "HORA_DE_INICIO": HOURS,
+    "HORA_DE_CIERRE": HOURS,
+}
