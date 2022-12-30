5b77cfdd6c8d154a72563ce9b60c0f83870ca126
jedzill4 <raulonco@gmail.com>
2022-12-30 13:49:21 +0000

    solve references error

--- src/aymurai/aymurai/utils/entities.py
+++ src/aymurai/aymurai/utils/entities.py
@@ -1,19 +1,10 @@
-import os
-import hashlib
 import pkgutil
-from pathlib import Path
 from copy import deepcopy
 
 import spacy
-import srsly
-from spacy.language import Language
-from spacy.tokens import Span, DocBin
 
 from aymurai.logging import get_logger
-from aymurai.meta.types import DataBlock
-from aymurai.spacy.components.extensions import AYMURAI_SPAN_ATTRS
-
-SPACY_CACHE_BASEPATH = os.getenv("SPACY_CACHE_BASEPATH", "/resources/cache/spacy")
+from aymurai.meta.api_interfaces import DocLabelAttributes
 
 logger = get_logger(__name__)
 
@@ -26,7 +17,7 @@ def format_entity(entity: spacy.tokens.Span, offset: int = 10) -> dict:
     soffset = max(0, start - offset)
     eoffset = min(len(entity.doc), end + offset)
 
-    attrs = {attr: getattr(entity._, attr, None) for attr in AYMURAI_SPAN_ATTRS}
+    attrs = DocLabelAttributes(aymurai_label=entity.label_).dict()
 
     return {
         "start": entity.start,
@@ -91,50 +82,3 @@ def load_ents(
             doc.spans[spans_key] = []
         doc.spans[spans_key] += list(ents)
     return doc
-
-
-def dataset_to_docbin(
-    nlp: Language,
-    dataset: DataBlock,
-    use_cache: bool = True,
-    cache_basepath: str | Path = SPACY_CACHE_BASEPATH,
-    categories: list[str] = [],
-    load_ents_as_spans: bool = False,
-) -> str:
-    empty_cats = {cat: 0 for cat in categories}
-
-    cache_key = hashlib.md5(srsly.json_dumps(dataset).encode("utf-8")).hexdigest()
-    logger.info(f"dataset hash: {cache_key}")
-    cache_path = f"{cache_basepath}/datasets/{cache_key}.spacy"
-    if use_cache and Path(cache_path).exists():
-        logger.info(f"loading docbin dataset from {cache_path}")
-        return cache_path
-
-    docbin = DocBin()
-    for item in dataset:
-        doc = nlp.make_doc(item["data"]["doc.text"])
-
-        # setup categories
-        if categories:
-            item_cats = item["data"].get("doc-cats", {})
-            item_cats = {k: v for k, v in item_cats.items() if k in categories}
-            doc.cats = empty_cats | item_cats
-
-        # setup entities
-        if hasattr(item["data"], "entities"):
-            ents = [
-                Span(doc, anno["start"], anno["end"], label=anno["label"])
-                for anno in item["data"]["entities"]
-            ]
-            if load_ents_as_spans:
-                doc.spans["sc"] = ents
-            else:
-                doc.ents = ents
-
-        docbin.add(doc)
-
-    logger.info(f"saving docbin dataset on {cache_path}")
-    os.makedirs(os.path.dirname(cache_path), exist_ok=True)
-    docbin.to_disk(cache_path)
-
-    return cache_path
