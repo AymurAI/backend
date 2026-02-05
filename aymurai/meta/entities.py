807b3c667a22e52542f76a80022cd4c77b184f84
jansaldo <julianansaldo@gmail.com>
2026-02-05 13:32:50 +0000

    Remove unused relations field from CanonicalEntity class for LLM inference phase

--- aymurai/meta/entities.py
+++ aymurai/meta/entities.py
@@ -103,11 +103,6 @@ class CanonicalEntity(BaseModel):
     attributes: dict[str, Any] = Field(
         default_factory=dict, description="Additional metadata for the entity"
     )
-    # TODO: We comment this because we won't use it in this LLM inference phase.
-    # relations: list[EntityRelation] = Field(
-    #     default_factory=list,
-    #     description="References to relations involving this entity",
-    # )
 
     @model_validator(mode="after")
     def validate_entity_id(self) -> CanonicalEntity:
