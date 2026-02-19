55de08685d1f497b7a32deed4b5867ed8ee3273c
conrabeatriz <conrabeatriz@gmail.com>
2026-02-19 08:54:33 -0300

    🔥 Removed aymurai/llm_providers for release/v1.5.0 compatibility

--- aymurai/llm_providers/__init__.py
+++
@@ -1,9 +0,0 @@
-from aymurai.llm_providers.ollama_provider import OllamaLLMProvider
-from aymurai.llm_providers.provider import DocumentChunk, LLMProvider, LLMResponse
-
-__all__ = [
-    "DocumentChunk",
-    "LLMProvider",
-    "LLMResponse",
-    "OllamaLLMProvider",
-]
